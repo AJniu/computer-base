a1352f6086e29e5e7b1ab631eafee09b30b84602
AJniu <aj_niu@163.com>
2024-04-08 19:38:15 +0800

    add:learn IndexedDB

--- frontend/esayIDB/index.js
+++ frontend/esayIDB/index.js
@@ -1,3 +1,19 @@
+// 定义变量存储数据库
+let db;
 // 打开数据库
-const request = indexedDB.open('myDB', 3)
-console.log(request)
+const request = indexedDB.open('myDB');
+// 打开IndexedDB成功回调
+request.onsuccess = (e) => {
+    // 结果e.target.result  - IDBDatabase 实例对象
+    console.log(e.target.result);
+    db = e.target.result;
+    // 针对此数据库的请求的所有错误的通用处理器
+    db.onerror = (e) => {
+        console.error(`数据库错误：${e.target.errorCode}`);
+    };
+};
+// 打开IndexedDB错误回调
+request.onerror = (e) => {
+    console.log(e.target.error);
+};
+// console.log(request)
